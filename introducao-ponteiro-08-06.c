ebe0c244d3f74d00a1f8b26e17bfa415527aa851
Luiz Gustavo <52093662+lgmbx@users.noreply.github.com>
2020-06-27 14:51:33 -0300

    Update introducao-ponteiro-08-06.c
    funcao deletar registro

--- introducao-ponteiro-08-06.c
+++ introducao-ponteiro-08-06.c
@@ -16,11 +16,10 @@ typedef struct {
 
 Dados dados;
 
+
 //ponteiro para file
 FILE *fptr;
 
-//contador de registros
-int contadorRegistro = 0;
 
 
 
@@ -69,7 +68,7 @@ void inserir() {
 
             fwrite(&dados, sizeof(Dados),1,fptr);
 
-            contadorRegistro++;
+
 
             printf("Gravado com sucesso...Continuar ? [S/N] \n");
             scanf("%c", &resp);
@@ -85,28 +84,139 @@ void inserir() {
 void exibir() {
 
     //usa o ponteiro para abrir o arquivo
-    fptr = fopen("arquivo","rb");
+    fptr = fopen("arquivo.txt","rb");
 
     //enquanto o fread ler 1 elemento e retornar a quantidade de elementos lidos
-    while(fread(&dados, sizeof(Dados),1, fptr) == 1){
+    while(fread(&dados, sizeof(Dados),1, fptr)){
         printf("%d, %s, %s, %s, %d, %.2f\n", dados.codigo, dados.nome, dados.sobrenome, dados.endereco, dados.telefone, dados.salario);
     }
 
     //usa o ponteiro para fechar o arquivo
     fclose(fptr);
-    system("pause");
+
+}
+
+void pesquisar(){
+    fptr = fopen("arquivo.txt", "rb");
+
+    char nomePesquisa[20];
+    printf("Digite o nome a ser pesquisado: ");
+    gets(nomePesquisa);
+    fflush(stdin);
+
+    while(fread(&dados, sizeof(Dados),1, fptr)){
+        if(strcmp(nomePesquisa, dados.nome) == 0){
+            printf("\n Nome........: %s",dados.nome);
+            printf("\n Sobrenome...: %s",dados.sobrenome);
+            printf("\n Endereço....: %s",dados.endereco);
+            printf("\n Telefone....: %d",dados.telefone);
+            printf("\n Salario.....: %.2f \n",dados.salario);
+        }
+    }
+
+    fclose(fptr);
+}
+
+void alterar(){
+
+
+    fptr = fopen("arquivo.txt", "rb+");
+
+    int contador = 0;
+    char numstr[10];
+
+    char nomePesquisa[20];
+    printf("Digite o nome a ser pesquisado: ");
+    gets(nomePesquisa);
+    fflush(stdin);
+
+    fread(&dados, sizeof(Dados), 1, fptr);
+
+    while(!feof(fptr) && strcmp(nomePesquisa, dados.nome) != 0){
+        fread(&dados, sizeof(Dados), 1, fptr);
+        contador++;
+    }
+    if(feof(fptr)){
+        printf("\nNao encontrado!\n");
+    }
+    else{
+        fseek(fptr, contador * sizeof(Dados), SEEK_SET);
+
+        printf("\n Nome...: %s \n", dados.nome);
+
+        printf("\n DIGITE NOVO Sobrenome...: %s \n", dados.sobrenome);
+        gets(dados.sobrenome);
+        fflush(stdin);
+
+
+        printf("\n DIGITE NOVO Endereco....: %s \n", dados.endereco);
+        gets(dados.endereco);
+        fflush(stdin);
+
+
+        printf("\n DIGITE NOVO Telefone....: %d \n",dados.telefone);
+        gets(numstr);
+        dados.telefone=atoi(numstr);
+        fflush(stdin);
+
+
+        printf("\n DIGITE NOVO Salario.....: %.2f\n",dados.salario);
+        gets(numstr);
+        dados.salario=atof(numstr);
+        fflush(stdin);
+
+
+        fwrite(&dados, sizeof(Dados),1,fptr);
+
+        printf("Registro alterado com sucesso...\n");
+    }
+
+    fclose(fptr);
+}
+
+
+void excluir(){
+    FILE *temp;
+
+    fptr = fopen("arquivo.txt", "rb+");
+    temp = fopen("temp.txt", "w+b");
+
+    int codigoPesquisa = 0;
+
+    printf("Digite o CODIGO DO CLIENTE para exclui-lo do registro: ");
+    scanf("%d", &codigoPesquisa);
+    fflush(stdin);
+
+
+    while(fread(&dados, sizeof(Dados), 1, fptr)){
+
+        if(codigoPesquisa != dados.codigo){
+            fwrite(&dados, sizeof(Dados),1, temp);
+        }
+    }
+
+    fclose(temp);
+    fclose(fptr);
+
+    remove("arquivo.txt");
+
+    rename("temp.txt", "arquivo.txt");
+
+
 }
 
 int main() {
 
 
     int op;
-    while(op != 4) {
+    while(op != 6) {
         system("cls");
         printf("[1] Inserir novo registro...\n");
         printf("[2] Exibir os registros...\n");
         printf("[3] Pesquisar registros...\n");
-        printf("[4] Sair do sistema...\n");
+        printf("[4] Alterar registro...\n");
+        printf("[5] Excluir registro...\n");
+        printf("[6] Sair do sistema...\n");
 
         printf("Informe a opcao desejada...");
         scanf("%d", &op);
@@ -121,10 +231,18 @@ int main() {
             getchar();
             break;
         case 3:
-            printf("Funcao em desenvolvimento...");
+            pesquisar();
+
+
             getchar();
             break;
         case 4:
+            alterar();
+            break;
+        case 5:
+            excluir();
+            break;
+        case 6:
             break;
         default:
             printf("Opcao invalida");
